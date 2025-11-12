04ba1abf901e6bf726e60dce3c091978075e4c78
Harold Camargo <hscamargo2002@gmail.com>
2025-11-11 20:13:34 -0500

    backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://sistemas-transaccionales-backend-service-389205780371.us-east1.run.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
